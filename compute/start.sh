a93e47183a807990bfeea6cf0847d110006c1263
Marcus G K Williams <marcus@mgkwill.com>
2016-12-21 12:17:58 -0800

    Update start.sh

--- compute/start.sh
+++ compute/start.sh
@@ -7,15 +7,15 @@ echo nameserver 8.8.8.8 | sudo tee -a /etc/resolv.conf
 # Start SSH Service
 sudo service ssh start
 
+# Start ovsbd-server
+sudo service openvswitch-switch start 
+
 echo -e $STACK_PASS | sudo passwd stack
 
 # Add/Configure local.conf - requires use of 'docker run -v :/home/stack/mnt
-chown stack:stack /home/stack/mnt/local.conf
+sudo chown stack:stack /home/stack/mnt/local.conf
 cp /home/stack/mnt/local.conf /home/stack/devstack/local.conf
 
-# sed -i "s/SERVICE_HOST=/SERVICE_HOST=$SERV_HOST/" /home/stack/devstack/local.conf
-
 ip=`/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1`;  echo "HOST_IP=$ip" >> /home/stack/devstack/local.conf
-# echo "HOST_IP=$IP" >> /home/stack/devstack/local.conf
-/home/stack/devstack/stack.sh
 
+/home/stack/devstack/stack.sh
