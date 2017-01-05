2fa6681551b2c4b172d2bbc63aabbb2eb54d36ec
Matt Welch <mattw0308@gmail.com>
2017-01-04 17:47:33 -0800

    feat: enable automatic reconfig of local.conf
    Enable host modification of local.conf.  Should be in the container.
    Minor mods to local.conf comments to reflect the positive case, rather
    than using a double-negative to explain the requirements.
    Modifications to start.sh to put some options in variables.

--- compute/run_compute.sh
+++ compute/run_compute.sh
@@ -4,18 +4,39 @@
 # dependencies: assumes proxy variables are defined in the local environment
 
 HOST_ID=01
-COMP_ID=01
+COMP_ID=02
+NAME=compute.${HOST_ID}.${COMP_ID}
 BASE_IMAGE=s3p/compute:latest
+ODL_NETWORK=true
 COMMAND="/home/stack/start.sh"
+CAPABILITIES="--privileged --cap-add ALL "
 
-if [ -z "$1" ] ; then
+if [ -n "$1" ] ; then
+    echo "Command argument supplied, running \"$1\" in $NAME..."
     COMMAND="$1"
 fi
 
-NAME=compute.${HOST_ID}.${COMP_ID}
+# modify the local.conf according to ODL_NETWORK value
+CONF_PATH="./local.conf"
+echo "Preparing $CONF_PATH for ODL=$ODL_NETWORK"
+echo
+if [ "$ODL_NETWORK" == "false" ] ; then 
+    # prepare local.conf to NOT use ODL networking (default to Neutron)
+    sed -i "s:^\(enable_plugin networking-odl\):#\1:g" $CONF_PATH
+    sed -i "s:^\(ODL_MODE=compute\):#\1:g" $CONF_PATH
+    sed -i "s:^\(ENABLED_SERVICES=\).*:\1n-cpu,q-agt:g" $CONF_PATH
+else
+    # prepare local.conf to use ODL networking
+    sed -i "s:^#\(enable_plugin networking-odl\):\1:g" $CONF_PATH
+    sed -i "s:^#\(ODL_MODE=compute\):\1:g" $CONF_PATH
+    sed -i "s:^\(ENABLED_SERVICES=\).*:\1n-cpu:g" $CONF_PATH
+fi
 
 docker run -it --name ${NAME} --hostname ${NAME} \
     --env http_proxy=$http_proxy --env https_proxy=$https_proxy \
-    $BASE_IMAGE \
+    --env ODL_NETWORK=$ODL_NETWORK \
+    --env STACK_PASS=stack \
+    --env SERV_HOST=192.168.0.5 \
+    $CAPABILITIES $BASE_IMAGE \
     $COMMAND
 
--- compute/start.sh
+++ compute/start.sh
@@ -3,7 +3,12 @@
 #  example 'docker run -e "STACK_PASS=stack" -e "SERV_HOST=192.168.0.5" compute'
 # or overided below by uncommenting:
 #STACK_PASS="stack" 
-#SERV_HOST="192.168.0.5"
+SERV_HOST="192.168.0.5"
+# ODL_NETWORK should be set in the 'docker run' script
+set -o nounset # throw an error if a variable is unset to prevent unexpected behaviors
+ODL_NETWORK=${ODL_NETWORK}
+DEVSTACK_HOME="/home/stack/devstack"
+CONF_PATH=$DEVSTACK_HOME/local.conf
 
 #Set Nameserver to google
 echo nameserver 8.8.8.8 | sudo tee -a /etc/resolv.conf
@@ -14,14 +19,15 @@ sudo service ssh start
 # Start openvswitch
 sudo service openvswitch-switch start 
 
-echo -e $STACK_PASS | sudo passwd stack
+# START HERE: the following line throws an error 
+echo "stack:$STACK_PASS" | sudo chpasswd 
 
 # Add/Configure local.conf - requires use of 'docker run -v :/home/stack/mnt
-sudo chmod 766 /home/stack/local.conf
-cp /home/stack/local.conf /home/stack/devstack/local.conf
+#sudo chmod 766 /home/stack/local.conf
+#cp /home/stack/local.conf $CONF_PATH
 
-sed -i "s/SERVICE_HOST=/SERVICE_HOST=$SERV_HOST/" /home/stack/devstack/local.conf
+sed -i "s/SERVICE_HOST=.*/SERVICE_HOST=$SERV_HOST/" $CONF_PATH
 
-ip=`/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1`;  echo "HOST_IP=$ip" >> /home/stack/devstack/local.conf
+ip=`/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1`;  echo "HOST_IP=$ip" >> $CONF_PATH
 
-/home/stack/devstack/stack.sh
+$DEVSTACK_HOME/stack.sh
