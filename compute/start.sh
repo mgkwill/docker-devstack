825f5733a22ed7aa4f9e1ad36f11d4661c6a278f
Matt Welch <mattw0308@gmail.com>
2017-01-11 12:39:58 -0800

    style: remove spaces @ EOL
    Removed annoying spaces at ends of lines.

--- compute/start.sh
+++ compute/start.sh
@@ -2,7 +2,7 @@
 # On docker run, Env Variables "STACK_PASS & SERV_HOST" should be set using -e
 #  example 'docker run -e "STACK_PASS=stack" -e "SERV_HOST=192.168.0.5" compute'
 # or overided below by uncommenting:
-#STACK_PASS="stack" 
+#STACK_PASS="stack"
 SERV_HOST="192.168.0.5"
 # ODL_NETWORK should be set in the 'docker run' script
 set -o nounset # throw an error if a variable is unset to prevent unexpected behaviors
@@ -17,16 +17,16 @@ echo nameserver 8.8.8.8 | sudo tee -a /etc/resolv.conf
 sudo service ssh start
 
 # Start openvswitch
-sudo service openvswitch-switch start 
+sudo service openvswitch-switch start
 
-# START HERE: the following line throws an error 
-echo "stack:$STACK_PASS" | sudo chpasswd 
+# START HERE: the following line throws an error
+echo "stack:$STACK_PASS" | sudo chpasswd
 
 # Add/Configure local.conf - TODO: requires use of 'docker run -v :/home/stack/mnt
 # modify the local.conf according to ODL_NETWORK value
 echo "Preparing $CONF_PATH for ODL=$ODL_NETWORK"
 echo
-if [ "$ODL_NETWORK" == "false" ] ; then 
+if [ "$ODL_NETWORK" == "false" ] ; then
     # prepare local.conf to NOT use ODL networking (default to Neutron)
     sed -i "s:^\(enable_plugin networking-odl\):#\1:g" $CONF_PATH
     sed -i "s:^\(ODL_MODE=compute\):#\1:g" $CONF_PATH
--- key-value/stop_key_key_value_server.sh
+++ key-value/stop_key_key_value_server.sh
@@ -3,6 +3,6 @@ _hostname=consul
 _container_name=$_hostname
 _image=progrium/consul
 
-docker stop $_container_name 
+docker stop $_container_name
 
 docker ps | grep $_container_name
