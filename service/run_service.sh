870bd487a3ed8d4c59721ae2ae060c6da71dc0be
Matt Welch <matt.welch@intel.com>
2017-01-30 10:06:39 -0800

    feat: enable stacking in Neutron-enabled service node
    * Neutron node now stacks successfully.  ODL is still having issues.
    * Allow container customization of configuration with changes to:
        * Dockerfile - move conf file
        * run_service.sh - don't mount files
        * Mods to local.conf to enable stacking
        * general cleanup of run and start scripts
    * remove pip workaround as it was fixed upstream

--- service/run_service.sh
+++ service/run_service.sh
@@ -9,14 +9,22 @@ IMAGE_VERSION="v0.1"
 IMAGE_NAME="${IMAGE_REPO}:${IMAGE_VERSION}"
 
 # image configuration 
-NAME=service-node
-ODL_NETWORK=true
+ODL_NETWORK=false
 CAPABILITIES="--privileged --cap-add ALL --cap-add NET_ADMIN --cap-add NET_RAW"
-SERV_HOST=172.17.0.2
+SERV_HOST=172.17.0.4
 STACK_PASS=stack
 # define _no_proxy based on the cluster topology
 _no_proxy=localhost,10.0.0.0/8,192.168.0.0/16,172.17.0.0/16,127.0.0.0/8
 
+CONF_FILE="$(pwd)/service.odl.local.conf"
+if [ "$ODL_NETWORK" = "false" ] ; then
+#    CONF_FILE="$(pwd)/service.neutron.local.conf"
+    echo "Using no-ODL (Neutron) local.conf ($CONF_FILE}"
+    NAME=service-node-neutron-2
+else
+    echo "Using OpenDaylight for local.conf ($CONF_FILE})"
+    NAME=service-node-odl
+fi
 if [ -n "$1" ] ; then
     # if a command is specified as an argument to the script, use it, 
     # else, default to the CMD defined in the Dockerfile
@@ -26,15 +34,6 @@ fi
 
 echo "Starting up docker container from image ${IMAGE_NAME}"
 echo "name: $NAME"
-
-CONF_FILE="$(pwd)/service.odl.local.conf"
-if [ ! "$ODL_NETWORK" ] ; then 
-    CONF_FILE="$(pwd)/service.neutron.local.conf"
-    echo "Using no-ODL (Neutron) local.conf ($CONF_FILE}"
-else
-    echo "Using OpenDaylight for local.conf ($CONF_FILE})"
-fi
-
 docker run -it --name ${NAME} --hostname ${NAME} --env TZ=America/Los_Angeles \
     --env JAVA_HOME=/usr/lib/jvm/java-8-oracle --env JAVA_MAX_MEM=16g \
     --env http_proxy=$http_proxy --env https_proxy=$https_proxy \
@@ -42,10 +41,7 @@ docker run -it --name ${NAME} --hostname ${NAME} --env TZ=America/Los_Angeles \
     --env ODL_NETWORK=$ODL_NETWORK \
     --env STACK_PASS=$STACK_PASS \
     -v /dev:/dev -v /lib/modules:/lib/modules \
-    -v $CONF_FILE:/home/stack/devstack/local.conf \
-    -v $(pwd)/start.sh:/home/stack/start.sh \
     -v $(pwd)/logs:/opt/stack/logs \
-    -v $(pwd)/workaround_requirements_error.sh:/home/stack/fix_reqs.sh \
     $CAPABILITIES \
     $IMAGE_NAME \
     $COMMAND
--- service/start.sh
+++ service/start.sh
@@ -9,37 +9,50 @@ set -o nounset # throw an error if a variable is unset to prevent unexpected beh
 ODL_NETWORK=${ODL_NETWORK}
 DEVSTACK_HOME="/home/stack/devstack"
 CONF_PATH=$DEVSTACK_HOME/local.conf
+BRANCH_NAME=newton
+TAG_NAME="origin/stable/${BRANCH_NAME}"
 
 #Set Nameserver to google
 echo nameserver 8.8.8.8 | sudo tee -a /etc/resolv.conf
 
-## Start SSH Service
-#sudo service ssh start
-#
-## Start openvswitch
-#sudo service openvswitch-switch start
-
-# START HERE: the following line throws an error
+# change the stack user password
 echo "stack:$STACK_PASS" | sudo chpasswd
 
-# get container IP 
+# get container IP
 ip=`/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1`
 # fix address binding issue in mysql
-sudo sed -i 's:^bind-address = .*:bind-address = ${ip}:' /etc/mysql/my.cnf
-
-# update the ip of this host
-# NOTE: BOTH of the following lines throw an error when this script is run, 
-# + likely because the local.conf is mounted from the host.  
-#sudo sed -i "s:\(HOST_IP=\).*:\1${ip}:" $CONF_PATH
-#sudo sed -i "s:\(SERVICE_HOST=\).*:\1${ip}:" $CONF_PATH
+sudo sed -i 's:^bind-address.*:#&:' /etc/mysql/my.cnf
 
 # allow services to start
 sudo sed -i 's:^exit .*:exit 0:' /usr/sbin/policy-rc.d
 
-# set the correct branch in devstack 
+# set the correct branch in devstack
 cd $DEVSTACK_HOME
-git checkout -b newton -t origin/stable/newton
+git checkout -b ${BRANCH_NAME} -t ${TAG_NAME}
 
-# begin stacking 
+# copy local.conf into devstack and customize, based on environment including:
+# ODL_NETWORK, ip, DEVSTACK_HOME, SERV_HOST
+cp /home/stack/local.conf $CONF_PATH
+
+# Configure local.conf
+# update the ip of this host
+sed -i "s:\(HOST_IP=\).*:\1${ip}:" $CONF_PATH
+sed -i "s:\(SERVICE_HOST=\).*:\1${ip}:" $CONF_PATH
+# modify the local.conf according to ODL_NETWORK value
+echo "Preparing $CONF_PATH for ODL=$ODL_NETWORK"
+echo
+if [ "$ODL_NETWORK" = "false" ] ; then
+    # prepare local.conf to NOT use ODL networking (default to Neutron)
+    sed -i "s:^#\(enable_service q-agt\).*:\1:g" $CONF_PATH
+    sed -i "s:^\(enable_plugin networking-odl\):#\1:g" $CONF_PATH
+    sed -i "s:^\(Q_ML2_PLUGIN_MECHANISM_DRIVERS=opendaylight\).*:#\1:g" $CONF_PATH
+else
+    # prepare local.conf to use ODL networking
+    sed -i "s:^\(enable_service q-agt\):#\1:g" $CONF_PATH
+    sed -i "s:^#\(enable_plugin networking-odl\):\1:g" $CONF_PATH
+    sed -i "s:^#\(Q_ML2_PLUGIN_MECHANISM_DRIVERS=opendaylight\).*:\1:g" $CONF_PATH
+fi
+
+# begin stacking
 $DEVSTACK_HOME/stack.sh
 
