cfd81d3d3e6ad42d2f7adbcf7c81e9b7c9e7e72e
Matt Welch <matt.welch@intel.com>
2017-02-02 14:18:32 -0800

    fix: fix apparmor profile to enable compute stacking
    Add 'docker-unconfined' apparmor profile to remove libvirtd errors.
    Removed annoying whitespace at EOL.
    More configuration detail in local.conf.
    Added no_proxy environment vars to run script.
    Reorganized start script and added some functionality to clean up start.
    
    This commit enables successful stacking of a non-ODL compute node and
    connection to a pre-defined control node through docker0 bridge.

--- compute/run_compute.sh
+++ compute/run_compute.sh
@@ -3,14 +3,23 @@
 # info: spawns a docker compute image
 # dependencies: assumes proxy variables are defined in the local environment
 
+# image selection
+IMAGE_REPO="s3p/compute"
+IMAGE_VERSION="v0.1"
+IMAGE_NAME="${IMAGE_REPO}:${IMAGE_VERSION}"
+
+# image configuration
 HOST_ID=01
-COMP_ID=07
+COMP_ID=09
 NAME=compute-${HOST_ID}-${COMP_ID}
-BASE_IMAGE=s3p/compute:working
 ODL_NETWORK=false
-COMMAND="/home/stack/start.sh"
 CAPABILITIES="--privileged --cap-add ALL --security-opt apparmor=docker-unconfined "
 MOUNTS="-v /dev:/dev -v /lib/modules:/lib/modules "
+STACK_PASS=stack
+SERV_HOST=172.17.0.3
+# TODO dynamically configure SERV_HOST
+# define _no_proxy based on the cluster topology
+_no_proxy=localhost,10.0.0.0/8,192.168.0.0/16,172.17.0.0/16,127.0.0.0/8
 
 if [ -n "$1" ] ; then
     echo "Command argument supplied, running \"$1\" in $NAME..."
@@ -19,10 +28,11 @@ fi
 
 docker run -it --rm --name ${NAME} --hostname ${NAME} \
     --env http_proxy=$http_proxy --env https_proxy=$https_proxy \
+    --env no_proxy=$_no_proxy \
     --env ODL_NETWORK=$ODL_NETWORK \
-    --env STACK_PASS=stack \
-    --env SERV_HOST=172.17.0.3 \
+    --env STACK_PASS=$STACK_PASS \
+    --env SERV_HOST=$SERV_HOST \
     $MOUNTS \
-    $CAPABILITIES $BASE_IMAGE \
+    $CAPABILITIES $IMAGE_NAME \
     $COMMAND
 
--- compute/start.sh
+++ compute/start.sh
@@ -3,28 +3,42 @@
 #  example 'docker run -e "STACK_PASS=stack" -e "SERV_HOST=192.168.0.5" compute'
 # or overided below by uncommenting:
 #STACK_PASS="stack"
-#SERV_HOST="192.168.0.5"
+# SERV_HOST="192.168.0.5"
 # ODL_NETWORK should be set in the 'docker run' script
 set -o nounset # throw an error if a variable is unset to prevent unexpected behaviors
-#ODL_NETWORK=${ODL_NETWORK}
+ODL_NETWORK=${ODL_NETWORK}
 DEVSTACK_HOME="/home/stack/devstack"
 CONF_PATH=$DEVSTACK_HOME/local.conf
-
-# allow services to start # This may not be a good idea
-#sudo sed -i 's:^exit .*:exit 0:' /usr/sbin/policy-rc.d
+BRANCH_NAME=stable/newton
+TAG_NAME="origin/stable/${BRANCH_NAME}"
 
 #Set Nameserver to google
 echo nameserver 8.8.8.8 | sudo tee -a /etc/resolv.conf
 
-# Start SSH Service
-#sudo service ssh start
-
-# Start openvswitch
-#sudo service openvswitch-switch start
-
-# START HERE: the following line throws an error
+# change the stack user password
 echo "stack:$STACK_PASS" | sudo chpasswd
 
+# get container IP
+ip=`/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1`
+
+# Start SSH Service
+sudo service ssh start
+
+# Start openvswitch
+sudo service openvswitch-switch start
+
+# set the correct branch in devstack
+cd $DEVSTACK_HOME
+git checkout -b ${BRANCH_NAME} -t ${TAG_NAME}
+
+# copy local.conf into devstack and customize, based on environment including:
+# ODL_NETWORK, ip, DEVSTACK_HOME, SERV_HOST
+cp /home/stack/local.conf $CONF_PATH
+
+# Configure local.conf
+# update the ip of this host & SERVICE_HOST
+sed -i "s/HOST_IP=.*/HOST_IP=${ip}/" $CONF_PATH
+sed -i "s/SERVICE_HOST=.*/SERVICE_HOST=$SERV_HOST/" $CONF_PATH
 # modify the local.conf according to ODL_NETWORK value
 echo "Preparing $CONF_PATH for ODL=$ODL_NETWORK"
 echo
@@ -39,11 +53,6 @@ else
     sed -i "s:^#\(ODL_MODE=compute\):\1:g" $CONF_PATH
     sed -i "s:^\(ENABLED_SERVICES=\).*:\1n-cpu:g" $CONF_PATH
 fi
-
-ip=`/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1`
-sed -i "s:\(HOST_IP=\).*:\1${ip}:" $CONF_PATH
-sed -i "s:\(SERVICE_HOST=\).*:\1${SERV_HOST}:" $CONF_PATH
-
 
 $DEVSTACK_HOME/stack.sh
 
